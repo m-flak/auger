488b2a2ee57e0f9c861bc77dcdfdba5d9d2cef54
Matthew Kehrer <matt91.mek@gmail.com>
2019-12-19 13:14:23 -0600

    new function in utils.html

--- auger/utils/__init__.py
+++ auger/utils/__init__.py
@@ -1,3 +1,3 @@
 from .html import (
-    clear_body_and_insert
+    clear_body_and_insert, append_to_body
 )
--- auger/utils/html.py
+++ auger/utils/html.py
@@ -5,13 +5,24 @@ def clear_body_and_insert(html, insert_content, insert_tag):
     Appends <insert_tag>insert_content</insert_tag> to the body.
     """
     document = lxml.html.fromstring(html)
-    insert_me = lxml.html.Element(insert_tag)
-
-    insert_me.text = insert_content
 
     for kid in document.body.getchildren():
         document.body.remove(kid)
 
-    document.body.append(insert_me)
+    return append_to_body(document, insert_content, insert_tag)
+
+def append_to_body(html, append_content, append_tag):
+    """Appends <append_tag>append_content</append_tag> to an HTML string or
+    an lxml document.
+    """
+    if isinstance(html, lxml.html.HtmlElement):
+        document = html
+    else:
+        document = lxml.html.fromstring(html)
+
+    append_me = lxml.html.Element(append_tag)
+    append_me.text = append_content
+
+    document.body.append(append_me)
 
     return lxml.html.tostring(document, method='html', encoding='unicode')
